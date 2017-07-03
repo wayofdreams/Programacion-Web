31da687d2664aaaaa86de145d9ff13114dd2ba97
wayofdreams <wayofdreams@users.noreply.github.com>
2017-07-03 18:27:06 -0400

    Dev (#4)
    * Create DomTreeMaker.js
    
    * Rename Web/public/DomTreeMaker.js to Web/public/scripts/DomTreeMaker.js
    
    * Update custom.css
    
    * Create TreeTest.handlebars
    
    * Rename TreeTest.handlebars to treetest.handlebars
    
    * Add a DOM tree test page
    
    * Add DOM Tree button.
    
    * Implemented about and a few other things.
    
    * Cleaned a few things

--- Web/app.js
+++ Web/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var exphbs  = require('express-handlebars');
- 
+
 var app = express();
 var helpers = require("./lib/helpers");
 
@@ -9,19 +9,27 @@ var hbs = exphbs.create({
 	defaultLayout: "main",
 	layoutDir: __dirname + "/views/layouts",
 	partialDir: __dirname + "/views/partials",
-    helpers: helpers, 
+    helpers: helpers,
 });
 
-app.use("/static",express.static(__dirname + "/public")); 
+app.use("/static",express.static(__dirname + "/public"));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
- 
+
 app.get('/', function (req, res) {
     res.render('home');
 });
 
+app.get('/tabletest', function (req, res) {
+    res.render('tabletest');
+});
+
 app.get('/about', function (req, res) {
     res.render('about');
+});
+
+app.get('/domtree', function (req, res) {
+    res.render('treetest');
 });
 
 app.listen(5884);
