cc1acad3713bcd80fc47dcd03c505f2cdecbd32e
Rafael Contreras <rak18@hotmail.com>
2017-07-24 18:54:28 -0400

    Fixed urls

--- PizzaSite/pizzapp.js
+++ PizzaSite/pizzapp.js
@@ -36,4 +36,17 @@ pizzapp.get('/', function (req, res) {
 
 pizzapp.use('/', routes);
 
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/myproject';
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+
+  db.close();
+});
+
 pizzapp.listen(5884);
